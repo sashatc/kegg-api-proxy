dfba4f7b4c3cd6080d51e907b757bb3756b89c9f
Oleksandr Chernenko <needsnoreason@Oleksandrs-Air.attlocal.net>
2025-06-21 19:40:58 -0700

    second version

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/kegg/*', async (req, res) => {
+// Use wildcard forwarding for all /kegg/... routes
+app.use('/kegg', async (req, res) => {
   try {
-    const keggUrl = `https://rest.kegg.jp${req.originalUrl.replace('/kegg', '')}`;
+    const keggPath = req.originalUrl.replace(/^\/kegg/, '');
+    const keggUrl = `https://rest.kegg.jp${keggPath}`;
     const response = await axios.get(keggUrl);
     res.set('Access-Control-Allow-Origin', '*');
     res.send(response.data);
@@ -14,4 +16,4 @@ app.get('/kegg/*', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Proxy running on port ${PORT}`));
+app.listen(PORT, () => console.log(`KEGG proxy running on port ${PORT}`));
